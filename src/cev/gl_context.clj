033bede713d4fe7a35be8ab02d264872709a7fcf
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-07-10 02:46:16 +0200

    Reload shaders

--- src/cev/gl_context.clj
+++ src/cev/gl_context.clj
@@ -14,22 +14,50 @@
 
 (def indices (int-array [0 1 2]))
 
+(defonce state (atom {}))
+
+(defn- draw! [{:keys [window program mesh]}]
+  (GL11/glClearColor 0.0 0.0 0.0 0.0)
+  (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
+  (shader/use program)
+  (vertex-array/draw-mesh mesh)
+  (GLFW/glfwSwapBuffers window)
+  (GLFW/glfwPollEvents))
+
+(defn- key-callback [window key scancode action mods]
+  (println "key-event" :key key :scancode scancode :action action :mods mods)
+
+  (when (= action GLFW/GLFW_RELEASE)
+    (condp = key
+      GLFW/GLFW_KEY_ESCAPE
+      (GLFW/glfwSetWindowShouldClose window true)
+
+      GLFW/GLFW_KEY_R
+      (swap! state assoc :program
+             (shader/load "triangle"))
+
+      nil)))
+
 (defn run!
   [width height]
   (try
-    (let [window (window/init width height "let there be triangles")
+    (let [window (window/init
+                  {::window/width width
+                   ::window/height height
+                   ::window/title "let there be triangles"
+                   ::window/key-callback key-callback})
           program (shader/load "triangle")
           mesh (vertex-array/create-mesh program vertices indices)]
-      (println "PROGRAM" program)
+
+
+      (swap! state assoc
+             :window window
+             :program program
+             :mesh mesh)
 
       (GL11/glEnable GL11/GL_DEPTH_TEST)
       (while (not (GLFW/glfwWindowShouldClose window))
-        (GL11/glClearColor 0.0 0.0 0.0 0.0)
-        (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
-        (shader/use program)
-        (vertex-array/draw-mesh mesh)
-        (GLFW/glfwSwapBuffers window)
-        (GLFW/glfwPollEvents))
+        (draw! @state))
 
       (shader/delete program)
       (GLFW/glfwDestroyWindow window))
@@ -39,9 +67,6 @@
       (System/exit 0))))
 
 (comment
-  (future (Thread/sleep 5000)
-          (println "Loading shader")
-          (shader/load))
   (a/go
     (a/<! (a/timeout 5000))
     (println "Running block")
--- src/cev/window.clj
+++ src/cev/window.clj
@@ -1,22 +1,18 @@
 (ns cev.window
-  (:import [org.lwjgl.opengl GL GL11]
-           [org.lwjgl.glfw GLFW GLFWErrorCallback GLFWKeyCallback]))
+  (:import
+   [org.lwjgl.glfw GLFW GLFWErrorCallback GLFWKeyCallback]
+   [org.lwjgl.opengl GL GL11]))
 
-(defn set-key-callback [window]
-  (println "ESCAPE KEY=" GLFW/GLFW_KEY_ESCAPE)
-  (println "Release code=" GLFW/GLFW_KEY_ESCAPE GLFW/GLFW_RELEASE)
+(defn set-key-callback [window callback]
   (GLFW/glfwSetKeyCallback
    window
    ;; Technically this was '.free'd upon exit, not sure if necessary.
    (proxy [GLFWKeyCallback] []
      (invoke [window key scancode action mods]
-       (println "GOT KEY" key scancode action mods)
-       (when (and (= key GLFW/GLFW_KEY_ESCAPE)
-                  (= action GLFW/GLFW_RELEASE))
-         (GLFW/glfwSetWindowShouldClose window true))))))
+       (callback window key scancode action mods)))))
 
 (defn init
-  [width height title]
+  [{::keys [width height title key-callback]}]
   ;; Technically this callback was .free'd up before exiting, not sure if necessary
   (GLFW/glfwSetErrorCallback (GLFWErrorCallback/createPrint System/err))
 
@@ -38,7 +34,7 @@
     (when-not window
       (throw (RuntimeException. "Failed to create the GLFW window")))
 
-    (set-key-callback window)
+    (set-key-callback window key-callback)
 
     (let [vidmode (GLFW/glfwGetVideoMode (GLFW/glfwGetPrimaryMonitor))]
       (GLFW/glfwSetWindowPos
@@ -51,10 +47,5 @@
 
     (GL/createCapabilities)
     (println "OpenGL version:" (GL11/glGetString GL11/GL_VERSION))
-    #_(GL11/glMatrixMode GL11/GL_PROJECTION)
-    #_(GL11/glOrtho 0.0 width
-                  0.0 height
-                  -1.0 1.0)
-    #_(GL11/glMatrixMode GL11/GL_MODELVIEW)
 
     window))
