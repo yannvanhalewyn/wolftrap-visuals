70be7b82f37354b4b06ce0dd68c7a2af526a3708
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-07-19 18:03:10 +0200

    Add cider-nrepl middleware and remove :local-dev

--- dev/cev/dev/nrepl.clj
+++ dev/cev/dev/nrepl.clj
@@ -2,10 +2,13 @@
   (:require
    [cev.main :as main]
    [clojure.java.io :as io]
-   [nrepl.server :as nrepl]))
+   [nrepl.server :as nrepl]
+   [cider.nrepl]))
 
 (defn -main [& _args]
   (spit ".nrepl-port" 7888)
-  (nrepl/start-server :port 7888)
+  (nrepl/start-server
+   :port 7888
+   :handler cider.nrepl/cider-nrepl-handler)
   (apply main/-main _args)
   (io/delete-file ".nrepl-port"))
