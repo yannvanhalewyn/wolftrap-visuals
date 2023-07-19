32027841bdbde83eb2bc86adf283a1b7c21b82bc
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2023-07-19 03:28:56 +0200

    Extract scene data from gl.context

--- src/cev/gl/context.clj
+++ src/cev/gl/context.clj
@@ -11,38 +11,6 @@
    [org.lwjgl.glfw GLFW]
    [org.lwjgl.opengl GL11]))
 
-(def fractal-canvas
-  (entity/make
-   {:entity/name "Fractal Canvas"
-    :mesh/vertices
-    [-1.0 -1.0
-     -1.0  1.0
-     1.0  1.0
-     1.0 -1.0]
-    :mesh/indices
-    [0 1 2 0 2 3]
-
-    :glsl/vertex-source (shader/resource-file "canvas.vert")
-    :glsl/fragment-source (shader/resource-file "distance_fractal.frag")
-    :glsl/attributes
-    [{:glsl/name "pos" :glsl/dimensions 2}]}))
-
-(def rgb-triangle
-  (entity/make
-   {:entity/name "RGB Triangle"
-    :mesh/vertices
-    [-1.0 -1.0 0.0 1.0 0.0 0.0
-     0.0  1.0 0.0 0.0 1.0 0.0
-     1.0 -1.0 0.0 0.0 0.0 1.0]
-    :mesh/indices
-    [0 1 2]
-
-    :glsl/vertex-source (shader/resource-file "playground.vert")
-    :glsl/fragment-source (shader/resource-file "playground.frag")
-    :glsl/attributes
-    [{:glsl/name "vpos" :glsl/dimensions 3}
-     {:glsl/name "vcol" :glsl/dimensions 3}]}))
-
 (defn- draw! []
   (let [window (db/get :window)
         [width height] (window/get-size window)]
@@ -61,35 +29,14 @@
     (GLFW/glfwSwapBuffers window)
     (GLFW/glfwPollEvents)))
 
-(defn- key-callback [window key scancode action mods]
-  (println "key-event" :key key :scancode scancode :action action :mods mods)
-
-  (when (= action GLFW/GLFW_RELEASE)
-    (condp = key
-      GLFW/GLFW_KEY_Q
-      (GLFW/glfwSetWindowShouldClose window true)
-
-      GLFW/GLFW_KEY_R
-      (doseq [entity (db/entities)]
-        (when-let [new-entity (entity/re-compile! entity)]
-          (db/add-entity! new-entity)))
-
-      nil)))
-
 (defn run!
-  [width height]
+  [window-opts entities]
   (try
-    (let [window (window/init
-                  {::window/width width
-                   ::window/height height
-                   ::window/title "Wolftrap Visuals"
-                   ::window/key-callback key-callback})
-          entity1 (entity/compile! fractal-canvas)
-          entity2 (entity/compile! rgb-triangle)]
-
+    (let [window (window/init window-opts)]
       (db/set-window! window)
-      (db/add-entity! entity1)
-      (db/add-entity! entity2)
+
+      (doseq [entity entities]
+        (db/add-entity! (entity/compile! entity)))
 
       (GL11/glEnable GL11/GL_DEPTH_TEST)
       (while (not (GLFW/glfwWindowShouldClose window))
@@ -102,9 +49,3 @@
       (GLFW/glfwTerminate)
       (shutdown-agents)
       (System/exit 0))))
-
-(comment
-  (a/go
-    (a/<! (a/timeout 5000))
-    (println "Running block")
-    (shader/load "blue")))
--- src/cev/main.clj
+++ src/cev/main.clj
@@ -1,17 +1,78 @@
 (ns cev.main
+  (:refer-clojure :exclude [run!])
   (:require
    [cev.db :as db]
    [cev.gl.context :as gl.context]
+   [cev.gl.entity :as entity]
+   [cev.gl.shader :as shader]
+   [cev.gl.window :as window]
    [cev.midi :as midi]
    [clojure.java.io :as io]
-   [nrepl.server :as nrepl]))
+   [nrepl.server :as nrepl])
+  (:import
+   [org.lwjgl.glfw GLFW]))
 
 (defn- handle-midi! [msg]
   (db/handle-midi! msg))
+
+(def fractal-canvas
+  (entity/make
+   {:entity/name "Fractal Canvas"
+    :mesh/vertices
+    [-1.0 -1.0
+     -1.0  1.0
+     1.0  1.0
+     1.0 -1.0]
+    :mesh/indices
+    [0 1 2 0 2 3]
+
+    :glsl/vertex-source (shader/resource-file "canvas.vert")
+    :glsl/fragment-source (shader/resource-file "distance_fractal.frag")
+    :glsl/attributes
+    [{:glsl/name "pos" :glsl/dimensions 2}]}))
+
+(def rgb-triangle
+  (entity/make
+   {:entity/name "RGB Triangle"
+    :mesh/vertices
+    [-1.0 -1.0 0.0 1.0 0.0 0.0
+     0.0  1.0 0.0 0.0 1.0 0.0
+     1.0 -1.0 0.0 0.0 0.0 1.0]
+    :mesh/indices
+    [0 1 2]
+
+    :glsl/vertex-source (shader/resource-file "playground.vert")
+    :glsl/fragment-source (shader/resource-file "playground.frag")
+    :glsl/attributes
+    [{:glsl/name "vpos" :glsl/dimensions 3}
+     {:glsl/name "vcol" :glsl/dimensions 3}]}))
+
+(defn- key-callback [window key scancode action mods]
+  (println "key-event" :key key :scancode scancode :action action :mods mods)
+
+  (when (= action GLFW/GLFW_RELEASE)
+    (condp = key
+      GLFW/GLFW_KEY_Q
+      (GLFW/glfwSetWindowShouldClose window true)
+
+      GLFW/GLFW_KEY_R
+      (doseq [entity (db/entities)]
+        (when-let [new-entity (entity/re-compile! entity)]
+          (db/add-entity! new-entity)))
+
+      nil)))
+
+(defn- run! [width height]
+  (gl.context/run!
+   {::window/width width
+    ::window/height height
+    ::window/title "Wolftrap Visuals"
+    ::window/key-callback key-callback}
+   [rgb-triangle fractal-canvas]))
 
 (defn -main [& _args]
   (spit ".nrepl-port" 7888)
   (nrepl/start-server :port 7888)
   (midi/add-listener! handle-midi!)
-  (gl.context/run! 800 600)
+  (run! 800 600)
   (io/delete-file ".nrepl-port"))
